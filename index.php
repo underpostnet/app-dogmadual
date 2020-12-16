d58687a9e681e7878ab84523d72e607af13e9147
fcoverdugoa <fcoverdugoa@underpost.net>
2020-12-16 13:07:08 -0400

    Update modules nexodev path

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 include 'C:/xampp/htdocs/nexodev/modules/dogmadual.php';
-$str_data = file_get_contents('C:/xampp/htdocs/nexodev/draft/json/dogmadual.json');
+$str_data = file_get_contents('C:/dd/global_data/json/dogmadual/dogmadual.json');
 $uri = $_SERVER['REQUEST_URI'];
 renderPath($uri, $str_data, 0);
 
