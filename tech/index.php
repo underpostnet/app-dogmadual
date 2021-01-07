4c5e34765e08fc493fb417f341dededf1ad7bca5
fcoverdugoa <fcoverdugoa@underpost.net>
2021-01-07 15:53:50 -0400

    update fw path

--- tech/index.php
+++ tech/index.php
@@ -1,6 +1,6 @@
 <?php
 
-include 'C:/xampp/htdocs/nexodev/modules/dogmadual.php';
+include 'C:/dd/deploy_area/nexodev/modules/dogmadual.php';
 $str_data = file_get_contents('C:/dd/global_data/json/dogmadual/dogmadual.json');
 $uri = $_SERVER['REQUEST_URI'];
 renderPath($uri, $str_data, 0);
